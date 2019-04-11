c9a95571e960f2ec51b8bc9f0821325404410854
Mitsichury <mitsichury@gmail.com>
2019-04-11 11:20:51 +0200

    Add init.py

--- nlp/lambda_function.py
+++ nlp/lambda_function.py
@@ -4,26 +4,26 @@ from botocore.vendored import requests
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-## ----- TODO : Change this with your instance ip address ----- ##
-URL = "http://34.244.201.243:5001/board"
-## ------------------------------------------------------------ ##
+URL = "http://54.246.250.50:5001/board"
+
 
 def cell_slot_to_number(cell_slot):
     row, column = 1, 1  # Default is middle
-    
+
     if 'top' in cell_slot:
         row = 0
     elif 'bottom' in cell_slot:
         row = 2
-    
+
     if 'left' in cell_slot:
         column = 0
     elif 'right' in cell_slot:
         column = 2
-        
-    cell_number = row*3 + column
+
+    cell_number = row * 3 + column
     return cell_number
-    
+
+
 def cell_number_to_slot(cell_number):
     slots = [
         "top-left", "top", "top-right",
@@ -32,12 +32,13 @@ def cell_number_to_slot(cell_number):
     ]
     return slots[cell_number]
 
+
 def handle_play(event):
     ## ----- TODO : Build the application logic with the backend and answer to the user ----- ##
-    cell = event['currentIntent']['slots']["Cell"]  # Something similar to get the cell told by the user (You should change "Cell" by the name of your slot)
-    message = "handle_play function not implemented yet."
+    cell = event['currentIntent']['slots']["Choice"]
+    message = "You want to play " + str(cell)
     ## -------------------------------------------------------------------------------------- ##
-    
+
     return {
         'sessionAttributes': event['sessionAttributes'],
         'dialogAction': {
@@ -49,11 +50,23 @@ def handle_play(event):
             }
         }
     }
-    
+
+
 def handle_restart(event):
     ## ----- TODO : Restart the game and answer to the user ----- ##
-    pass
+    return {
+        'sessionAttributes': event['sessionAttributes'],
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': 'Fulfilled',
+            'message': {
+                'contentType': 'PlainText',
+                'content': "Not done yet"
+            }
+        }
+    }
     ## ---------------------------------------------------------- ##
+
 
 def handle_default(event):
     return {
@@ -63,7 +76,7 @@ def handle_default(event):
             'fulfillmentState': 'Fulfilled',
             'message': {
                 'contentType': 'PlainText',
-                'content': "Intent processing not implemented in lambda yet."
+                'content': "Choix par défaut y a qwack dans la marre mon carnard "
             }
         }
     }
@@ -78,12 +91,11 @@ def lambda_handler(event, context):
     logger.debug('event.bot.name={}'.format(event['bot']['name']))
     logger.debug('userId={}'.format(event['userId']))
     logger.debug('intentName={}'.format(event['currentIntent']['name']))
+    logger.debug(event)
 
-    ## ----- TODO :  Change the checks by those of your intents ----- ##
-    if event['currentIntent']['name'] == "CellToPlay":
+    if event['currentIntent']['name'] == "inquire_choice":
         return handle_play(event)
-    elif event['currentIntent']['name'] == "RestartGame":
+    elif event['currentIntent']['name'] == "Retry":
         return handle_restart(event)
-    ## -------------------------------------------------------------- ##
     else:
         return handle_default(event)
